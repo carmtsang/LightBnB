780ca4a9fee0d5b5f082d2c2a4de4220e46d2103
Carmen Tsang <carmtsang89@hotmail.com>
2022-06-09 02:54:02 +0000

    add properties_review table

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -36,3 +36,12 @@ CREATE TABLE reservations (
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
+
+CREATE TABLE property_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
+);
