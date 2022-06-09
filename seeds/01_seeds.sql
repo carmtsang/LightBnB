638e181c0d10d80df353c2d2d30e7827a98b7f57
Carmen Tsang <carmtsang89@hotmail.com>
2022-06-09 03:19:06 +0000

    add data for 3 users

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (name, email, password)
+VALUES ('Leeba Ashish', 'leeba@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Grover Luka', 'gluka@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Hari Cintia', 'hcintia@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
