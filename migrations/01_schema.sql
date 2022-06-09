06fbf9b39e9c9781288eceb54387e60235e540b1
Carmen Tsang <carmtsang89@hotmail.com>
2022-06-09 02:51:11 +0000

    add reso

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -28,3 +28,11 @@ CREATE TABLE properties (
   post_code VARCHAR(255) NOT NULL,
   active BOOLEAN NOT NULL DEFAULT TRUE
 );
+
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
