1b724065a954b1363fecd56f94695b957f0f437b
Carmen Tsang <carmtsang89@hotmail.com>
2022-06-09 04:08:21 +0000

    add 3 rows of property_reviews

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -9,6 +9,11 @@ VALUES (1, 'Speed lamp', 'description', 'https://images.pexels.com/photos/208667
 (3, 'Habit mix', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 46058, 0, 5, 6, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland And Labrador', '44583', true);
 
 INSERT INTO reservations (start_date, end_date, property_id, guest_id)
-VALUES  ('2018-09-11', '2018-09-26', 2, 3),
+VALUES  ('2018-09-11', '2018-09-26', 1, 3),
 ('2019-01-04', '2019-02-01', 2, 2),
-('2021-10-01', '2021-10-14', 1, 4);
+('2021-10-01', '2021-10-14', 3, 1);
+
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message) 
+VALUES (2, 2, 1, 5, 'message'),
+(1, 3, 3, 5, 'message'),
+(3, 1, 2, 5, 'message');
