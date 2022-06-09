952b61319d0ddc00d02ef686e9b9fdeecfd8ec6d
Carmen Tsang <carmtsang89@hotmail.com>
2022-06-09 04:00:59 +0000

    add 3 rows of reservation data

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -7,3 +7,8 @@ INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover
 VALUES (1, 'Speed lamp', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 930.61, 6, 4, 8, 'Canada', '536 Namsub Highway', 'Sotboske', 'Quebec', '28142', true),
 (1, 'Blank corner', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 85234, 6,  6, 7, 'Canada', '651 Nami Road', 'Bohbatev', 'Alberta', '83680', true),
 (3, 'Habit mix', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 46058, 0, 5, 6, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland And Labrador', '44583', true);
+
+INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+VALUES  ('2018-09-11', '2018-09-26', 2, 3),
+('2019-01-04', '2019-02-01', 2, 2),
+('2021-10-01', '2021-10-14', 1, 4);
