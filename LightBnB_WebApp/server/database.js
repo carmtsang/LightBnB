8eeda9a18b01fca211c92b8e46f481cb3bc3e4b3
Carmen Tsang <carmtsang89@hotmail.com>
2022-06-11 04:37:09 +0000

    connect to database

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,6 +1,17 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
+const { Pool } = require('pg');
+const pool = new Pool ({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
+pool.query(`SELECT title FROM properties LIMIT 10`)
+  .then(response => {console.log(response)});
+
 /// Users
 
 /**
