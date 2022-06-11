495de13280324acd504b61b0748854ef0d4a976b
Carmen Tsang <carmtsang89@hotmail.com>
2022-06-11 04:50:07 +0000

    refractor getAllProperties

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -9,8 +9,7 @@ const pool = new Pool ({
   database: 'lightbnb'
 });
 
-pool.query(`SELECT title FROM properties LIMIT 10`)
-  .then(response => {console.log(response)});
+// pool.query(`SELECT title FROM properties LIMIT 10`).then(response => {console.log(response)});
 
 /// Users
 
@@ -77,13 +76,15 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
-}
+const getAllProperties = (options, limit = 10) => {
+  pool.query(`SELECT * FROM properties LIMIT $1`, [limit])
+  .then((result) => {
+    console.log(result.rows);
+  })
+  .catch(err => {
+    console.log(err.message);
+  });
+};
 exports.getAllProperties = getAllProperties;
 
 
